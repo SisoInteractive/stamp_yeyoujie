785120431054f463dd15574764ca55e550112c4b
Qianglong Mo <sammok2003@hotmail.com>
2015-05-15 15:13:23 +0800

    Fix mistake.

--- dist/assets/scripts/main.js
+++ dist/assets/scripts/main.js
@@ -91,7 +91,7 @@ var app = {
         }
 
         //  this index recording the current scene number
-        app.curScene = 7;  // scene is letter 1 now cause 1
+        app.curScene = 1;  // scene is letter 1 now cause 1
 
         //  sceneMain
         // ------------------------------------------------
